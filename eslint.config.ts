7bb8b11a8728b411c3e1c0a71f55aa0444386e47
x3r1x <3opobep@gmail.com>
2025-10-02 08:43:38 +0300

    lab3.1 and lab3.2 done

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,12 +3,13 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   { 
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], 
     plugins: { js }, 
-    extends: ["js/recommended"], 
+    extends: ["js/recommended"],
     languageOptions: { globals: globals.browser },
     rules: {
       "no-duplicate-imports": "error",
@@ -21,4 +22,5 @@ export default defineConfig([
   tseslint.configs.strict,
   tseslint.configs.stylistic,
   // pluginReact.configs.flat.recommended,
-]);
+], eslintConfigPrettier
+);
